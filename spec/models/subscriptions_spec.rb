69986cb1a23adff80c3d8982e09d7c69746262f1
celedituro <cdituro@fi.uba.ar>
2022-09-22 15:17:29 -0300

    test: Adding subscription tests

--- spec/models/subscriptions_spec.rb
+++ spec/models/subscriptions_spec.rb
@@ -9,6 +9,13 @@ describe Subscription do
         expect(on_demand_sub.amount_to_pay(job_offers)).to eq 0.0
       end
 
+      it 'should be 0.0 when one inactive offers' do
+        job_offers = []
+        job_offers.append(JobOffer.new(title: 'Programmer vacancy', is_active: false))
+        on_demand_sub = described_class.new
+        expect(on_demand_sub.amount_to_pay(job_offers)).to eq 0.0
+      end
+
       it 'should be 10.0 when one active offers' do
         job_offers = []
         job_offers.append(JobOffer.new(title: 'Programmer vacancy', is_active: true))
@@ -26,6 +33,13 @@ describe Subscription do
         expect(professional_sub.amount_to_pay(job_offers)).to eq 30.0
       end
 
+      it 'should be 30.0 when one inactive offers' do
+        job_offers = []
+        job_offers.append(JobOffer.new(title: 'Programmer vacancy', is_active: false))
+        professional_sub = described_class.new
+        expect(professional_sub.amount_to_pay(job_offers)).to eq 30.0
+      end
+
       it 'should be 30.0 when one active offers' do
         job_offers = []
         job_offers.append(JobOffer.new(title: 'Programmer vacancy', is_active: true))
@@ -39,6 +53,13 @@ describe Subscription do
     describe 'amount_to_pay' do
       it 'should be 80.0 when no active offers' do
         job_offers = []
+        professional_sub = described_class.new
+        expect(professional_sub.amount_to_pay(job_offers)).to eq 80.0
+      end
+
+      it 'should be 80.0 when one inactive offers' do
+        job_offers = []
+        job_offers.append(JobOffer.new(title: 'Programmer vacancy', is_active: false))
         professional_sub = described_class.new
         expect(professional_sub.amount_to_pay(job_offers)).to eq 80.0
       end
