6e9b07a7c87f8d5e92339d93e4ffd5f39a1d3636
alejovillores <avillores@fi.uba.ar>
2022-09-24 18:09:36 -0300

    test: Adding total amount test
    Adding total amount test with multiples on-demand usersr

--- spec/models/report_maker_spec.rb
+++ spec/models/report_maker_spec.rb
@@ -1,9 +1,31 @@
 require 'spec_helper'
 
-def mock_multiple_job_offers_repo(user, number)
+def mock_multiple_job_offers_repo(user, num_jobs)
   job_repo = instance_double('offer_repo', all_active: [])
-  number.times { job_repo.all_active.push(JobOffer.new(title: 't', is_active: true, user_id: 1)) }
+  num_jobs.times { job_repo.all_active.push(JobOffer.new(title: 't', is_active: true, user_id: 1)) }
   allow(job_repo).to receive(:find_by_owner).with(user).and_return(job_repo.all_active)
+  job_repo
+end
+
+def mock_multiple_users(num_users, sub_type)
+  user_repo = instance_double('user_repo', users: [])
+  num_users.times do |user|
+    user_repo.users.push(User.new(id: user, email: "user_#{user}@gmail.com", subscription: sub_type))
+  end
+  user_repo
+end
+
+def mock_multiple_job_offers_repo_from_users(num_jobs, users)
+  job_repo = instance_double('offer_repo', all_active: [])
+  users.each do |user|
+    job_offers = []
+    num_jobs.times do
+      job = JobOffer.new(title: 't', is_active: true, user_id: user.id)
+      job_repo.all_active.push(job)
+      job_offers.push(job)
+    end
+    allow(job_repo).to receive(:find_by_owner).with(user).and_return(job_offers)
+  end
   job_repo
 end
 
@@ -90,6 +112,14 @@ describe ReportMaker do
       report_maker = described_class.new(mock_multiple_job_offers_repo(user, 1), user_repo)
 
       expect(report_maker.make_report.fetch(:items)[0].fetch(:amount_to_pay)).to eq 80.0
+    end
+
+    it 'should have a total_amout of 30.0 with three on-demand subs' do
+      users_repo = mock_multiple_users(3, 'on-demand')
+      offer_repo = mock_multiple_job_offers_repo_from_users(1, users_repo.users)
+      report_maker = described_class.new(offer_repo, users_repo)
+
+      expect(report_maker.make_report.fetch(:total_amount)).to eq 30.0
     end
   end
 end
